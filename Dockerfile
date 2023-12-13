e333fc7c067e267b9f6b71ffc639393413b21b7b
FallMaple <tony53517230@gmail.com>
2023-12-13 21:43:29 +0000

    feat: add Dockerfile, docker-compose.yml, entrypoint.sh

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:bookworm
+
+# Copy Web UI
+COPY src/ /app/
+WORKDIR /app
+RUN apt-get update -y && apt-get install -y wireguard net-tools iproute2 iptables
+RUN chmod u+x wgd.sh
+RUN ./wgd.sh install
+RUN mkdir config
+ENV CONFIGURATION_PATH=config
+ENV IN_DOCKER=1
+
+WORKDIR /app
+CMD ["/app/entrypoint.sh"]
---
+++ src/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+directory="/etc/wireguard"
+
+if [ -n $(ls -A $directory) ]; then
+    private=$(wg genkey)
+    public=$(echo $private | wg pubkey)
+    conf=wg0
+    int=eth0
+	cat > $directory/$conf.conf <<EOL
+[Interface]
+SaveConfig = true
+Address = $WG_ADDRESS
+ListenPort = $WG_PORT
+PrivateKey = $private
+
+PostUp = iptables -t nat -A POSTROUTING -s $WG_ADDRESS -o $int -j MASQUERADE
+PostUp = iptables -A INPUT -p udp -m udp --dport $WG_PORT -j ACCEPT
+PostUp = iptables -A FORWARD -i $conf -j ACCEPT
+PostUp = iptables -A FORWARD -o $conf -j ACCEPT
+
+PostDown = iptables -D INPUT -p udp -m udp --dport $WG_PORT -j ACCEPT
+PostDown = iptables -D FORWARD -i $conf -j ACCEPT
+PostDown = iptables -D FORWARD -o $conf -j ACCEPT
+PostDown = iptables -t nat -A POSTROUTING -s $WG_ADDRESS -o $int -j MASQUERADE
+EOL
+    chmod 700 $directory/$conf.conf
+fi
+
+cd /app && ./wgd.sh start
